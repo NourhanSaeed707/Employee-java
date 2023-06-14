5a406ee72d0d0fc2a1ee84e799e59ccedbfcfbc1
nourhan <nourhan.saeed@mobidevlabs.com>
2023-06-15 00:24:27 +0300

    first

---
+++ src/main/java/com/example/employeesystemapi/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.example.employeesystemapi.controller;
+
+import com.example.employeesystemapi.model.Employee;
+import com.example.employeesystemapi.services.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/employee")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping("/add")
+    public Employee createEmployee(@RequestBody Employee employee) {
+      return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping("/getAll")
+    public List<Employee> getAllEmployees () {
+        return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employee) {
+        employee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(employee);
+
+    }
+}
---
+++ src/main/java/com/example/employeesystemapi/services/EmployeeServiceImpl.java
@@ -0,0 +1,63 @@
+package com.example.employeesystemapi.services;
+
+import com.example.employeesystemapi.entity.EmployeeEntity;
+import com.example.employeesystemapi.model.Employee;
+import com.example.employeesystemapi.repository.EmployeeRepository;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    private EmployeeRepository employeeRepository;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+    @Override
+    public Employee createEmployee (Employee employee) {
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+        //to copy employee model to employee entity
+        BeanUtils.copyProperties(employee, employeeEntity);
+        employeeRepository.save(employeeEntity);
+        return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+        List<Employee> employees = employeeEntities.stream().map( emp -> new Employee(emp.getId(), emp.getFirstName(), emp.getLastName(), emp.getEmail()))
+                .collect(Collectors.toList());
+        return employees;
+    }
+
+    @Override
+    public boolean deleteEmployee(Long id) {
+        EmployeeEntity employee = employeeRepository.findById(id).get();
+        employeeRepository.delete(employee);
+        return true;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        employeeEntity.setEmail(employee.getEmail());
+        employeeEntity.setFirstName(employee.getFirstName());
+        employeeEntity.setLastName(employee.getLastName());
+        employeeRepository.save(employeeEntity);
+        return employee;
+    }
+
+
+}
